b64d7b1f8cea8ed536a27994c252e7c00bfe4f8a
JoonYoung2 <90246124+JoonYoung2@users.noreply.github.com>
2023-09-29 03:50:15 +0900

    testCode update

---
+++ library-app/library-app/src/test/kotlin/com/group/libraryapp/JunitTest.kt
@@ -0,0 +1,16 @@
+package com.group.libraryapp
+
+import org.junit.jupiter.api.Test
+
+class JunitTest {
+
+    @Test
+    fun test1(){
+        println("test1 실행")
+    }
+
+    @Test
+    fun test2(){
+        println("test2 실행")
+    }
+}
